f5c147b776ab42268d04f4b304c668449364126c
Charles Ellis <hamled@hamled.name>
2015-08-05 22:18:22 -0700

    Initial Rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LinkedListBlog::Application.config.secret_key_base = '3c59bee279a42055ddf73afe45b75906c8ed3f48ab3f23c20eff5dcc7083035c68ab670f4ed847ab1e4291cd9a9ac733f8546a32621112ad3a9c8e356e0c1e7b'
