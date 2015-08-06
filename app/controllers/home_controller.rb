644fe4ccc8d7cc622b4b37465ab75e96cbf4bd12
Charles Ellis <hamled@hamled.name>
2015-08-06 01:46:10 -0700

    Root route uses Home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,11 @@
 class HomeController < ApplicationController
+
+  def index
+    # Display our latest blog post
+    @post = Post.all.first || Post.new
+
+    # For now get all of the comments
+    # maybe implement paging later?
+    @comments = @post.comments || []
+  end
 end
